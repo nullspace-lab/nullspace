ac7a57f2dc492f28906d0b0718df1f98db3c604f
Yuri Mutti <muttiyuri@gmail.com>
2025-06-21 22:53:53 +0200

    feat: add base-ui

--- apps/web/src/routes/__root.tsx
+++ apps/web/src/routes/__root.tsx
@@ -1,4 +1,4 @@
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools/production'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools/production';
 import {
   Link,
   Outlet,
@@ -6,20 +6,21 @@ import {
   useRouterState,
   HeadContent,
   Scripts,
-} from '@tanstack/react-router'
-import { TanStackRouterDevtools } from '@tanstack/react-router-devtools'
-import * as React from 'react'
-import { Toaster } from 'react-hot-toast'
-import type { QueryClient } from '@tanstack/react-query'
-import { DefaultCatchBoundary } from '~/components/DefaultCatchBoundary'
-import { IconLink } from '~/components/IconLink'
-import { NotFound } from '~/components/NotFound'
-import appCss from '~/styles/app.css?url'
-import { seo } from '~/utils/seo'
-import { Loader } from '~/components/Loader'
+} from '@tanstack/react-router';
+import { TanStackRouterDevtools } from '@tanstack/react-router-devtools';
+import * as React from 'react';
+import { Toaster } from 'react-hot-toast';
+import type { QueryClient } from '@tanstack/react-query';
+import { DefaultCatchBoundary } from '~/components/DefaultCatchBoundary';
+import { IconLink } from '~/components/IconLink';
+import { NotFound } from '~/components/NotFound';
+import appCss from '~/styles/app.css?url';
+import { seo } from '~/utils/seo';
+import { Loader } from '~/components/Loader';
+import { Avatar } from '@base-ui-components/react/avatar';
 
 export const Route = createRootRouteWithContext<{
-  queryClient: QueryClient
+  queryClient: QueryClient;
 }>()({
   head: () => ({
     meta: [
@@ -64,18 +65,18 @@ export const Route = createRootRouteWithContext<{
       <RootDocument>
         <DefaultCatchBoundary {...props} />
       </RootDocument>
-    )
+    );
   },
   notFoundComponent: () => <NotFound />,
   component: RootComponent,
-})
+});
 
 function RootComponent() {
   return (
     <RootDocument>
       <Outlet />
     </RootDocument>
-  )
+  );
 }
 
 function RootDocument({ children }: { children: React.ReactNode }) {
@@ -85,64 +86,41 @@ function RootDocument({ children }: { children: React.ReactNode }) {
         <HeadContent />
       </head>
       <body>
-        <div className="h-screen flex flex-col min-h-0">
-          <div className="bg-slate-900 border-b border-slate-800 flex items-center justify-between py-4 px-8 box-border">
-            <div className="flex items-center gap-4">
-              <div>
-                <Link to="/" className="block leading-tight">
-                  <div className="font-black text-2xl text-white">Trellaux</div>
-                  <div className="text-slate-500">a TanStack Demo</div>
-                </Link>
-              </div>
-              <LoadingIndicator />
-            </div>
-            <div className="flex items-center gap-6">
-              {/* <label
-                htmlFor="countries"
-                className="block text-sm font-medium text-gray-900 dark:text-white"
-              >
-                Delay
-              </label>
-              <select
-                className="border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                onChange={(event) => {
-                  // setExtraDelay(Number(event.currentTarget.value))
-                }}
-                defaultValue="0"
-              >
-                <option value="0">None</option>
-                <option value="100">100</option>
-                <option value="500">500</option>
-                <option value="2000">2000</option>
-              </select> */}
-              <IconLink
-                href="https://github.com/TanStack/router/tree/main/examples/react/start-trellaux"
-                label="Source"
-                icon="/github-mark-white.png"
-              />
-              <IconLink
-                href="https://tanstack.com"
-                icon="/tanstack.png"
-                label="TanStack"
-              />
-            </div>
-          </div>
-
-          <div className="flex-grow min-h-0 h-full flex flex-col">
-            {children}
-            <Toaster />
-          </div>
-        </div>
+        <main className="h-screen flex flex-col min-h-0 root">
+          <Avatar.Root className="inline-flex size-12 items-center justify-center overflow-hidden rounded-full bg-gray-100 align-middle text-base font-medium text-black select-none">
+            <Avatar.Image
+              src="https://images.unsplash.com/photo-1543610892-0b1f7e6d8ac1?w=128&h=128&dpr=2&q=80"
+              width="48"
+              height="48"
+              className="size-full object-cover"
+            />
+            <Avatar.Fallback className="flex size-full items-center justify-center text-base">
+              LT
+            </Avatar.Fallback>
+          </Avatar.Root>
+          <Avatar.Root className="inline-flex size-12 items-center justify-center overflow-hidden rounded-full bg-gray-100 align-middle text-base font-medium text-black select-none">
+            LT
+          </Avatar.Root>
+          {/* <Link to="/" className="block leading-tight"></Link> */}
+          <LoadingIndicator />
+          {/* <IconLink
+            href="https://github.com/TanStack/router/tree/main/examples/react/start-trellaux"
+            label="Source"
+            icon="/github-mark-white.png"
+          /> */}
+          {children}
+          <Toaster />
+        </main>
         <ReactQueryDevtools />
         <TanStackRouterDevtools position="bottom-right" />
         <Scripts />
       </body>
     </html>
-  )
+  );
 }
 
 function LoadingIndicator() {
-  const isLoading = useRouterState({ select: (s) => s.isLoading })
+  const isLoading = useRouterState({ select: (s) => s.isLoading });
   return (
     <div
       className={`h-12 transition-all duration-300 ${
@@ -151,5 +129,5 @@ function LoadingIndicator() {
     >
       <Loader />
     </div>
-  )
+  );
 }
